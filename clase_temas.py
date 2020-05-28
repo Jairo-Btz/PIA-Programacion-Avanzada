3895ce6d90cb036f39dce8bd51b54af3fe091c07
Jairo Manuel Batrez Gonzalez <jairo_real11@hotmail.com>
2020-05-28 02:19:42 -0500

    clase_temas.py

---
+++ clase_temas.py
@@ -0,0 +1,12 @@
+class Temas:
+    def_init_(self,idtema,nombre)
+    self._idtema = idtema
+    self._nombre = nombre
+
+    @property
+    def idtema(self):
+        return self._idtema
+
+    @property
+    def nombre(self):
+        return self._nombre
